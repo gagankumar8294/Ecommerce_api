02e9ae580e0842c3825105b2ee52b569401bf9de
Gagan-Sky <YOUR_EMAIL>
2024-04-05 21:27:30 +0530

    Basic Auth integrated | changes requiree in | ProdutController

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import  express  from "express";
 import  productRouter  from './src/features/product/products.routes.js'
 import userRouter from "./src/features/user/user.routes.js";
 import bodyParser from "body-parser";
+import basicAuthorizer from "./src/middlewares/basicAuth.middleware.js"
 // Crate Server
 const server = express();
 server.use(bodyParser.json())
 
 // for all requests related to products, redirect to product routes.
-server.use('/api/products', productRouter);
+server.use('/api/products', basicAuthorizer, productRouter);
 server.use('/api/users', userRouter);
 
 
--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -23,9 +23,13 @@ export default class UserModel {
         const user = users.find((u) => u.email == email && u.password == password);
         return user;
     };
+
+    static getAll() {
+        return users;
+    }
 }
 
-var users = [{
+let users = [{
     "name": "seller User",
     "email": "seller@example.com",
     "password": "password",
