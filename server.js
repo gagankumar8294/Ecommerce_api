583deaa089558726a04a6cb1560dd14b783b3ccf
Gagan-Sky <YOUR_EMAIL>
2024-04-05 17:13:03 +0530

    Testing login / signup - Api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import  express  from "express";
 import  productRouter  from './src/features/product/products.routes.js'
+import userRouter from "./src/features/user/user.routes.js";
 import bodyParser from "body-parser";
 // Crate Server
 const server = express();
@@ -7,10 +8,13 @@ server.use(bodyParser.json())
 
 // for all requests related to products, redirect to product routes.
 server.use('/api/products', productRouter);
+server.use('/api/users', userRouter);
+
 
 server.get("/", (req, res) => {
     res.send("Welcome to Ecommerce Api");
 })
+
 
 server.listen(3200);
 
---
+++ src/features/user/user.model.js
@@ -0,0 +1,33 @@
+export default class UserModel {
+    constructor(name, email, password, type, id) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+        this.id = id;
+    }
+
+    static signUp(name, email, password, type){
+        const newUser = new UserModel(
+            name,
+            email,
+            password,
+            type
+        );
+        newUser.id = users.length+1;
+        users.push(newUser);
+        return newUser;
+    }
+
+    static signIn(email, password) {
+        const user = users.find((u) => u.email == email && u.password == password);
+        return user;
+    };
+}
+
+var users = [{
+    "name": "seller User",
+    "email": "seller@example.com",
+    "password": "password",
+    "type": "seller"
+}]
