5a1840e27fb46e8568987ca5c57c05732faacd35
gagankumar <gagankumar8294@gmail.com>
2024-04-26 14:37:48 +0530

    .

--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 const userController = new UserController();
 
 userRouter.post('/signup', (req, res) => { userController.signUp(req, res)});
-userRouter.post('/signin', (req, res) => {userController.signIn(req, res)});
+userRouter.post('/signin', (req, res) => { userController.signIn(req, res)});
 
 export default userRouter;
